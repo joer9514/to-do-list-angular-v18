7ed89a5a23de3a78f463ff47d8a37269afbab4e2
joer9514 <joer9514@gmail.com>
2024-05-29 09:48:14 -0500

    feat(imports): added new configuration in the imports paths

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { appConfig } from '@/config/app.config';
-import { AppComponent } from '@/ui/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from '@config/app.config';
+import { AppComponent } from '@ui/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
