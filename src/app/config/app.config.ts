67503db124b29c41a393bcfc7dfbf841bb031d8f
joer9514 <joer9514@gmail.com>
2024-05-29 09:46:21 -0500

    feat(environments): add firebase environments configurations

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
-
-import { routes } from '@/ui/pages/app.routes';
+import { environment } from '@environments/environment';
+import { routes } from '@ui/pages/app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
 };
