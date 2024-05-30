a97de5215ce4cb38e774fe0b1539b98d0097b025
joer9514 <joer9514@gmail.com>
2024-05-30 17:02:00 -0500

    feat(UI): added custom themes by angular material 3

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { TaskRepository } from '@core/task/domain/repositories/task.repository';
 import { TaskRepositoryImplementation } from '@core/task/infraestructure/repositories/task.repository.implementation';
@@ -12,9 +13,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    /* Architecture Providers */
     { provide: TaskRepository, useClass: TaskRepositoryImplementation },
   ],
 };
